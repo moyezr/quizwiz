7623cee58caf70ddfa139da8b212c7a7fe341c85
Moyez <moyezrabbani@gmail.com>
2024-01-09 13:45:43 +0530

    updated styles of sign-in button

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -2,20 +2,22 @@
 
 import { signIn } from "next-auth/react";
 import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 type Props = {};
 
 const SignInButton = (props: Props) => {
   return (
-    <Button
-      className="border border-gray-400 text-3xl bg-gradient-to-br from-transparent to-gray-300/20 backdrop-blur-xl px-8 rounded-full py-2 dark:text-gray-200 text-gray-700 tracking-wide hover:shadow-gray-500 hover:shadow-md transition "
+    <button
+      className={cn(
+        "border border-gray-400 text-3xl bg-gradient-to-br from-transparent to-gray-300/20 backdrop-blur-xl px-8 rounded-full py-2 dark:text-gray-200 text-gray-700 tracking-wide hover:shadow-gray-500 hover:shadow-md transition "
+      )}
       onClick={async () => {
         await signIn("google");
       }}
-      variant="default"
     >
       Play
-    </Button>
+    </button>
   );
 };
 
